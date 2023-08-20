22bb3c8d5067dbc533ad51c88638b8d02d5b79ff
Ian July <ianjuly4@gmail.com>
2023-08-20 17:55:26 -0600

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 10; 
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = (2,4,6,8,10,12,14,16,18,20);
+Math.max;
